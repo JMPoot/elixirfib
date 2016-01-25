646acbec81fb5c16ad3157d1e69f64947f1bdcc2
Thomas Farla <thomas-farla@hotmail.com>
2016-01-25 20:47:03 +0100

    Add test for negative integers

--- lib/elixirfib.ex
+++ lib/elixirfib.ex
@@ -1,5 +1,19 @@
 defmodule Elixirfib do
+  @doc """
+    Calculate the n-th position of the fibonacci sequence
+
+    ## Examples
+    iex> Elixirfib.fib(0)
+    0
+    iex> Elixirfib.fib(1)
+    1
+    iex> Elixirfib.fib(-1)
+    0
+  """
   def fib(0), do: 0
   def fib(1), do: 1
-  def fib(n), do: fib(n-1) + fib(n-2)
+  def fib(n) when n > 1 do
+    fib(n-1) + fib(n-2)
+  end
+  def fib(_), do: 0
 end
