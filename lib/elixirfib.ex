8aa42b8463c56e019c03738a9e9b1d54ba505315
Thomas Farla <thomas-farla@hotmail.com>
2016-01-25 20:41:39 +0100

    Force short notation

--- lib/elixirfib.ex
+++ lib/elixirfib.ex
@@ -1,9 +1,5 @@
 defmodule Elixirfib do
-
-  def fib(0) do 0 end
-  
-  def fib(1) do 1 end
-
-  def fib(n) do fib(n-1) + fib(n-2) end
-
+  def fib(0), do: 0
+  def fib(1), do: 1
+  def fib(n), do: fib(n-1) + fib(n-2)
 end
---
+++ lib/server.ex
@@ -0,0 +1,7 @@
+defmodule Elixirfib.Server do
+  use GenServer
+
+  def start do
+    
+  end
+end
